0395a321d57d7b91b580d1c8988643a8b13de395
Tex Villarreal <tex_villarreal@yahoo.com>
2022-04-17 15:12:14 -0500

    Added ui with rock paper scissors game

--- BotClient.py
+++ BotClient.py
@@ -6,7 +6,9 @@ from meme import meme_of_the_day
 from jokeapi import Jokes
 
 from discord.ext.commands import Bot, Context, CommandNotFound
-from discord import Intents, Message
+from discord import Intents
+
+from discord_ui import Button, UI, receive
 
 from asyncio import sleep
 from threading import Lock
@@ -16,6 +18,7 @@ import random
 # intents_input = Intents(members=True, dm_messages=True, guild_messages=True, guild_reactions=True, guilds=True, messages=True, typing=True)
 intents_input = Intents().all()
 bot = Bot(command_prefix='?', intents=intents_input)
+ui = UI(bot)
 
 @bot.event
 async def on_command_error(ctx: Context, error):
@@ -59,6 +62,41 @@ async def joke(ctx: Context):
         await sleep(3)
         await ctx.send(f'{joke["delivery"]}')
 
+@bot.command()
+async def rps(ctx: Context):
+    rock_id = '1'
+    paper_id = '2'
+    scissor_id = '3'
+    rock_str = 'Rock'
+    paper_str = 'Paper'
+    scissor_str = 'Scissor'
+    selected: receive.ButtonInteraction = await (
+        await ctx.message.channel.send("Rock, Paper, Scissors Duel!", components=[
+            Button(label=rock_str, custom_id=rock_id, color="red"),
+            Button(label=paper_str, custom_id=paper_id, color="green"),
+            Button(label=scissor_str, custom_id=scissor_id, color="blurple")
+        ])
+    ).wait_for("button", bot)
+    user_choice = selected.component.content
+    bot_choice = random.choice([scissor_str, rock_str, paper_str])
+    if bot_choice == user_choice:
+        await selected.respond(f'We both choice {bot_choice}.  Try again?')
+    else:
+        if bot_choice == 'Paper':
+            if user_choice == 'Rock':
+                await selected.respond(f'Paper beats Rock! Get gud, loser...')
+            else:
+                await selected.respond(f'Your Scissor beats my Paper! I concede...')
+        elif bot_choice == 'Rock':
+            if user_choice == 'Scissor':
+                await selected.respond(f'Rock beats Scissor! Get gud, loser...')
+            else:
+                await selected.respond(f'Your Paper beats my Rock! I concede...')
+        else: # Bot chose scissor
+            if user_choice == 'Paper':
+                await selected.respond(f'Scissor beats Paper! Get gud, loser...')
+            else:
+                await selected.respond(f'Your Rock beats my Scissor! I concede...')
 
 def get_bot():
     return bot
