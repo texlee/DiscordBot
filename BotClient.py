9e2bfa1e59f555c9a50db132ac37d1919cb7a4e0
Tex Villarreal <tex_villarreal@yahoo.com>
2022-04-18 21:04:20 -0500

    Adding stuff

--- BotClient.py
+++ BotClient.py
@@ -6,11 +6,11 @@ from meme import meme_of_the_day
 from jokeapi import Jokes
 
 from discord.ext.commands import Bot, Context, CommandNotFound
-from discord import Intents
+from discord import Intents, Member
 
 from discord_ui import Button, UI, receive
 
-from asyncio import sleep
+from asyncio import sleep, TimeoutError
 from threading import Lock
 import datetime
 import random
@@ -36,11 +36,20 @@ async def write_command_call_to_file(ctx: Context):
     lock: Lock = get_file_lock()
     lock.acquire()
     with open('command_calls.txt', 'a') as f:
-        f.write(f'{ctx.author.display_name} executed command: {ctx.invoked_with} at {datetime.datetime.today()}\n')
+        info_str = f'executed command: \'{ctx.invoked_with}\' at {datetime.datetime.today()}\n'
+        try:
+            author: Member = ctx.author
+            f.write(f'{author.nick} {info_str}')
+        except Exception:
+            try:
+                f.write(f'{author.display_name} {info_str}')
+            except Exception as e:
+                f.write(f'{str(author.id)} {info_str}')
+                pass
     lock.release()
 
 @bot.command()
-async def food(ctx: Context, arg = None):
+async def food(ctx: Context, arg = ''):
     food = get_food()
     if '-g' == arg:
         restaurant = random.choice(food.get_gluten_free_foods())
@@ -54,7 +63,6 @@ async def food(ctx: Context, arg = None):
 async def joke(ctx: Context):
     jokes = await Jokes()
     joke = await jokes.get_joke(blacklist=['racist'])
-    # joke = await jokes.get_joke(category=['dark'], blacklist=['nsfw', 'religious', 'political', 'racist'])
     if joke["type"] == "single": # Print the joke
         await ctx.send(f'{joke["joke"]}')
     else:
@@ -70,13 +78,18 @@ async def rps(ctx: Context):
     rock_str = 'Rock'
     paper_str = 'Paper'
     scissor_str = 'Scissor'
-    selected: receive.ButtonInteraction = await (
-        await ctx.message.channel.send("Rock, Paper, Scissors Duel!", components=[
-            Button(label=rock_str, custom_id=rock_id, color="red"),
-            Button(label=paper_str, custom_id=paper_id, color="green"),
-            Button(label=scissor_str, custom_id=scissor_id, color="blurple")
-        ])
-    ).wait_for("button", bot)
+    try:
+        selected: receive.ButtonInteraction = await (
+            await ctx.message.channel.send("Rock, Paper, Scissors Duel!", components=[
+                Button(label=rock_str, custom_id=rock_id, color="red"),
+                Button(label=paper_str, custom_id=paper_id, color="green"),
+                Button(label=scissor_str, custom_id=scissor_id, color="blurple")
+            ])
+        ).wait_for("button", client=bot, timeout=30.0)
+    except TimeoutError:
+        await ctx.send('Duel request has timed out. (30 seconds)')
+        return
+
     user_choice = selected.component.content
     bot_choice = random.choice([scissor_str, rock_str, paper_str])
     if bot_choice == user_choice:
